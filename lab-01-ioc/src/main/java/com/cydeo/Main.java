2bb135fb7fe390efb425c9d16b7f0cf99ed4cbe9
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-02-27 19:51:06 -0500

    added lab-01

--- lab-01-ioc/src/main/java/com/cydeo/Main.java
+++ lab-01-ioc/src/main/java/com/cydeo/Main.java
@@ -3,12 +3,10 @@ package com.cydeo;
 import com.cydeo.account.Current;
 import com.cydeo.account.Saving;
 import com.cydeo.config.Config;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
+
 public class Main {
 
     public static void main(String[] args) {
