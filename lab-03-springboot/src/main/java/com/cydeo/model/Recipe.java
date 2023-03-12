a4e32a2389042ae8d0c8f3d92b1d8e78377e13e6
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-12 13:32:03 -0400

    commit lab-03

---
+++ lab-03-springboot/src/main/java/com/cydeo/model/Recipe.java
@@ -0,0 +1,23 @@
+package com.cydeo.model;
+
+import com.cydeo.enums.RecipeType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class Recipe {
+
+    private UUID id;
+    private String name;
+    private int duration;
+
+    private String preparation;
+    private List<Ingredients> ingredients;
+
+    private RecipeType recipeType;
+
+}
