6dada09030aa941c7615809c8cfdc0bbc1c3242b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-09 16:33:41 -0400

    entity added

--- lab-06-orm/src/main/java/com/cydeo/entity/Cart.java
+++ lab-06-orm/src/main/java/com/cydeo/entity/Cart.java
@@ -1,33 +1,22 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.CartState;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
 import javax.persistence.*;
-import java.math.BigInteger;
 
 @Entity
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+public class Cart extends BaseEntity{
 
-public class Cart {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     @Enumerated(EnumType.STRING)
     private CartState cartState;
     @ManyToOne
-    @JoinColumn(name = "customer_id")
     private Customer customer;
     @ManyToOne
-    @JoinColumn(name = "discount_id")
     private Discount discount;
-    @OneToOne(mappedBy = "cart")
-    private Orders orders;
 
-    public Cart(CartState cartState) {
-        this.cartState = cartState;
-    }
 }
--- lab-06-orm/src/main/java/com/cydeo/entity/Category.java
+++ lab-06-orm/src/main/java/com/cydeo/entity/Category.java
@@ -1,21 +1,16 @@
 package com.cydeo.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
 
 @Entity
 @NoArgsConstructor
-@Data
-
-public class Category {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Getter
+@Setter
+public class Category extends BaseEntity{
     private String name;
 }
