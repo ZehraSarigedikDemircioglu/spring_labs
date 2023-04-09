cf722ce2b74815e481291adfa8afeea617ef20ce
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-08 22:55:29 -0400

    entity added

---
+++ lab-06-orm/src/main/java/com/cydeo/entity/Balance.java
@@ -0,0 +1,23 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigInteger;
+
+@Entity
+@NoArgsConstructor
+@Data
+
+public class Balance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double amount;
+    private BigInteger customerId;
+}
