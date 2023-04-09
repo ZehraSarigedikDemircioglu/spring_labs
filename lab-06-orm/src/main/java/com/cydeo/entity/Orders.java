1bb8c5316de9d54d861a4bc263ae31d52aa0eca6
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-09 01:16:22 -0400

    entity added

--- lab-06-orm/src/main/java/com/cydeo/entity/Orders.java
+++ lab-06-orm/src/main/java/com/cydeo/entity/Orders.java
@@ -3,18 +3,28 @@ package com.cydeo.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.math.BigInteger;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
 
 @Entity
 @NoArgsConstructor
 @Data
 public class Orders {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private BigDecimal paidPrice;
+    private BigDecimal totalPrice;
+    @ManyToOne
+    private Customer customer;
+    @OneToOne
+    private Cart cart;
+    @OneToOne
+    private Payment payment;
+
+    public Orders(BigDecimal paidPrice, BigDecimal totalPrice) {
+        this.paidPrice = paidPrice;
+        this.totalPrice = totalPrice;
+    }
 }
--- lab-06-orm/src/main/java/com/cydeo/entity/Product.java
+++ lab-06-orm/src/main/java/com/cydeo/entity/Product.java
@@ -3,11 +3,9 @@ package com.cydeo.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -20,4 +18,12 @@ public class Product {
     private BigDecimal price;
     private Integer quantity;
     private Integer remainingQuantity;
+    @OneToMany
+    private List<CartItem> cartItemList;
+
+    public Product(BigDecimal price, Integer quantity, Integer remainingQuantity) {
+        this.price = price;
+        this.quantity = quantity;
+        this.remainingQuantity = remainingQuantity;
+    }
 }
