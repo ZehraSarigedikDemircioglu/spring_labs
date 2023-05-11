b068052da2c030e679e0e20a81f7ccbb447ea254
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:06:17 -0400

    added entity

---
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Product.java
@@ -0,0 +1,30 @@
+package com.cydeo.lab07ormqueries.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product extends BaseEntity{
+
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer remainingQuantity;
+
+    @JoinTable(name = "product_category_rel",
+            joinColumns = @JoinColumn(name = "p_id"), inverseJoinColumns = @JoinColumn(name = "c_id"))
+    @ManyToMany
+    private List<Category> categoryList;
+}
