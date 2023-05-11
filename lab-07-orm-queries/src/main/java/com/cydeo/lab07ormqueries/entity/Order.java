10243398227b242d6ab47cb691866acff2bf149e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:33:00 -0400

    added entity

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Order.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Order.java
@@ -3,6 +3,7 @@ package com.cydeo.lab07ormqueries.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Table(name = "orders")
+@ToString
 public class Order extends BaseEntity {
     private BigDecimal paidPrice;
     private BigDecimal totalPrice;
