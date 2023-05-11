b70c400c6e658d02a7753a5d7369a3db5a507a03
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:32:45 -0400

    added entity

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Address.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Address.java
@@ -4,6 +4,7 @@ package com.cydeo.lab07ormqueries.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -12,6 +13,7 @@ import javax.persistence.ManyToOne;
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Address extends BaseEntity {
     private String name;
     private String street;
