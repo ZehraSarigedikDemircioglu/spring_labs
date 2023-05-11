18a2db753da19f7f9843fd04913246d02555f70b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:07:21 -0400

    added entity

---
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Discount.java
@@ -0,0 +1,24 @@
+package com.cydeo.lab07ormqueries.entity;
+
+import com.cydeo.enums.DiscountType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.math.BigDecimal;
+
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Discount extends BaseEntity{
+
+    private BigDecimal discount;
+    @Enumerated(EnumType.STRING)
+    private DiscountType discountType;
+    private String name;
+}
