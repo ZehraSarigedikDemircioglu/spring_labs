bb99b42c54de3564906fc42ae8140828f6575a4b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-11 12:05:05 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/BalanceRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/BalanceRepository.java
@@ -1,14 +1,21 @@
 package com.cydeo.lab07ormqueries.repository;
 
 import com.cydeo.lab07ormqueries.entity.Balance;
+import com.cydeo.lab07ormqueries.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
+
     //Write a derived query to check balance exists for specific customer
+    boolean existsBalanceByCustomer(Customer customer);
+
     //Write a derived query to get balance for specific customer
+    Balance getByCustomer_Id(Long id);
+
     //Write a native query to get top 5 max balance
+
     //Write a derived query to get all balances greater than or equal specific balance amount
     //Write a native query to get all balances less than specific balance amount
 }
