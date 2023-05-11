af7e4048c8fbaa66203d616908233b7cf5ff0d12
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:11:03 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/AddressRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/AddressRepository.java
@@ -1,9 +1,11 @@
 package com.cydeo.lab07ormqueries.repository;
 
+import com.cydeo.lab07ormqueries.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepository {
+public interface AddressRepository extends JpaRepository<Address, Long> {
     //Write a derived query to get all address with a specific customer
     //Write a derived query to get address with a specific street
     //Write a derived query to get top 3 address with a specific customer email
