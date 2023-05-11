6d6cbaa84d2d574dd7829dfc1c101d8894b51655
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:31:11 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/AddressRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/AddressRepository.java
@@ -1,15 +1,32 @@
 package com.cydeo.lab07ormqueries.repository;
 
 import com.cydeo.lab07ormqueries.entity.Address;
+import com.cydeo.lab07ormqueries.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
     //Write a derived query to get all address with a specific customer
+    List<Address> findAllByCustomer(Customer customer);
+
     //Write a derived query to get address with a specific street
+    List<Address> findByStreet(String street);
+
     //Write a derived query to get top 3 address with a specific customer email
+    List<Address> findTop3ByCustomer_Email(String email);
+
     //Write a derived query to get all address with a specific customer and name
+    List<Address> findAllByCustomerAndName(Customer customer, String name);
+
     //Write a derived query to list all address where the beginning of the street contains the keyword
+    List<Address> findAllByStreetStartingWith(String keyword);
+
     //Write a JPQL query to get all address with a specific customerId
+    @Query("select a from Address a where a.customer.id =?1")
+    List<Address> retrieveByCustomer_Id(Long id);
 }
