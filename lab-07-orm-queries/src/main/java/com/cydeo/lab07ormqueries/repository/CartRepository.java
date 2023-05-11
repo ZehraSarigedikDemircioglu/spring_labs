445db3606fe6666acf00b085c4f15413d7908ac4
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:31:32 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartRepository.java
@@ -1,8 +1,11 @@
 package com.cydeo.lab07ormqueries.repository;
+
+import com.cydeo.lab07ormqueries.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository {
+public interface CartRepository extends JpaRepository<Cart, Long> {
     //Write a derived query to get all cart by specific discount type
     //Write a JPQL query to get all cart by customer
     //Write a derived query to get all cart by customer and cart state
