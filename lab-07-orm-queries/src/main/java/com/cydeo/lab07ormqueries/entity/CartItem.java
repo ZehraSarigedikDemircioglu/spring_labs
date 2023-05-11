6f9e53c1aa0b0d82dfa1e832db6bc7e19401a173
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:05:38 -0400

    added entity

---
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/CartItem.java
@@ -0,0 +1,22 @@
+package com.cydeo.lab07ormqueries.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class CartItem extends BaseEntity{
+
+    private Integer quantity;
+    @ManyToOne
+    private Cart cart;
+    @ManyToOne
+    private Product product;
+}
