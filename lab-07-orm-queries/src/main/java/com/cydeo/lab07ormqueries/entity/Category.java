e41f59dbdab2aca3d6e3ace1fd4ba4bb70084603
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-10 21:32:55 -0400

    added entity

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Category.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/entity/Category.java
@@ -4,14 +4,15 @@ package com.cydeo.lab07ormqueries.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
-
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Category extends BaseEntity {
     private String name;
 }
