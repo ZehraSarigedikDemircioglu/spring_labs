cd8b940d4bad20ec895cb91811666006fc685cff
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-12 17:02:06 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/BalanceRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/BalanceRepository.java
@@ -4,6 +4,7 @@ import com.cydeo.lab07ormqueries.entity.Balance;
 import com.cydeo.lab07ormqueries.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -24,5 +25,8 @@ public interface BalanceRepository extends JpaRepository<Balance, Long> {
 
     //Write a derived query to get all balances greater than or equal specific balance amount
     List<Balance> getAllByAmountGreaterThanEqual(BigDecimal amount);
+
     //Write a native query to get all balances less than specific balance amount
+    @Query(value = "select * from balance where amount < ?1", nativeQuery = true)
+    List<Balance> getAllBalanceLessThanAmount(@Param("amount") BigDecimal amount);
 }
