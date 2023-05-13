3baebce45e149f874479c175c4bc389ddbe86f9a
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-13 12:27:22 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartItemRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartItemRepository.java
@@ -24,8 +24,9 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     List<CartItem> retrieveAllCartItemByCartStateAndProductName(@Param("cartState") String cartState, @Param("name") String name);
 
     //Write a native query to get cart items for specific cart state and without discount
-    @Query(value = "SELECT * FROM cart_item ci JOIN cart c ON  ci.cart_id = c.id WHERE c.cart_state=?1 AND c.discount_id IS NULL ", nativeQuery=true)
+    @Query(value = "select * from cart_item ci join cart c on  ci.cart_id = c.id where c.cart_state=?1 and c.discount_id is null", nativeQuery=true)
     List<CartItem> retrieveCartItemsByCartStateWithoutDiscount(@Param("cartState") String cartState);
+
     //Write a native query to get cart items for specific cart state and with specific Discount type
     @Query(value="SELECT * FROM cart_item ci JOIN cart c ON  ci.cart_id=c.id JOIN discount d ON c.discount_id =d.id " +
             "WHERE c.cart_state=?1 AND d.discount_type = ?2", nativeQuery = true)
