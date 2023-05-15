e31450df628dc22d91f51dc1c8275aabec19e2d7
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-14 22:18:43 -0400

    added repository

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartRepository.java
@@ -1,8 +1,10 @@
 package com.cydeo.lab07ormqueries.repository;
 
 import com.cydeo.lab07ormqueries.entity.Cart;
+import com.cydeo.lab07ormqueries.enums.CartState;
 import com.cydeo.lab07ormqueries.enums.DiscountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,8 +16,11 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findAllByDiscountDiscountType(DiscountType discountType);
 
     //Write a JPQL query to get all cart by customer
+    @Query("select c from Cart c WHERE c.customer.id = ?1")
+    List<Cart> retrieveCartListByCustomer(Long id);
 
     //Write a derived query to get all cart by customer and cart state
+    List<Cart> findAllByCustomerIdAndCartState(Long id, CartState cartState);
 
     //Write a derived query to get all cart by customer and cart state and discount is null condition
 
