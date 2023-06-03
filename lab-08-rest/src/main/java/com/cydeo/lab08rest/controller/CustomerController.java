50561bf0e9279ada7a97dec730ab8eb93db983de
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-03 17:19:21 -0400

    added customer controller

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/CustomerController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/CustomerController.java
@@ -1,4 +1,44 @@
 package com.cydeo.lab08rest.controller;
 
+import com.cydeo.lab08rest.dto.AddressDTO;
+import com.cydeo.lab08rest.dto.CustomerDTO;
+import com.cydeo.lab08rest.model.ResponseWrapper;
+import com.cydeo.lab08rest.service.CustomerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getCustomerList() {
+
+        return ResponseEntity
+                .ok(new ResponseWrapper("Customers are successfully retrieved", customerService.getAllCustomer(), HttpStatus.OK));
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateCustomer(@RequestBody CustomerDTO customerDTO) {
+        customerService.updateCustomer(customerDTO);
+        return ResponseEntity.ok(new ResponseWrapper("Customer is successfully updated", customerDTO, HttpStatus.CREATED));
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        customerService.createCustomer(customerDTO);
+        return ResponseEntity.ok(new ResponseWrapper("Customer is successfully created", customerDTO, HttpStatus.CREATED));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<ResponseWrapper> getCustomerByEmail(@PathVariable("email") String email) {
+        return ResponseEntity.ok(new ResponseWrapper("Customer is successfully retrieved", customerService.getCustomerByEmail(email), HttpStatus.OK));
+    }
 }
