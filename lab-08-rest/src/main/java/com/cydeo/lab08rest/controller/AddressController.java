2ceb62206922e6a8db3860aa7966150594e5e735
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-03 15:10:33 -0400

    added address controller

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/AddressController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/AddressController.java
@@ -3,8 +3,10 @@ package com.cydeo.lab08rest.controller;
 import com.cydeo.lab08rest.dto.AddressDTO;
 import com.cydeo.lab08rest.model.ResponseWrapper;
 import com.cydeo.lab08rest.service.AddressService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
@@ -21,7 +23,7 @@ public class AddressController {
     public ResponseEntity<ResponseWrapper> getAddressList() {
 
         return ResponseEntity
-                .ok(new ResponseWrapper("Address are successfully retrieved", addressService.getAllAddress()));
+                .ok(new ResponseWrapper("Addresses are successfully retrieved", addressService.getAllAddress(), HttpStatus.OK));
     }
 
     //    @PutMapping
@@ -30,28 +32,30 @@ public class AddressController {
 //        return ResponseEntity.status(HttpStatus.OK).build();
 //    }
     @PutMapping
-    public AddressDTO updateAddress(@PathVariable("id") Long id, @RequestBody AddressDTO addressDTO) {
-        return addressService.updateAddress(addressDTO);
+    public ResponseEntity<ResponseWrapper> updateAddress(@RequestBody AddressDTO addressDTO) {
+        addressService.updateAddress(addressDTO);
+        return ResponseEntity.ok(new ResponseWrapper("Address is successfully updated", addressDTO, HttpStatus.CREATED));
     }
 
     @PostMapping
-    public AddressDTO createAddress(@RequestBody AddressDTO addressDTO) {
-        return addressService.createAddress(addressDTO);
+    public ResponseEntity<ResponseWrapper> createAddress(@RequestBody AddressDTO addressDTO) {
+        addressService.createAddress(addressDTO);
+        return ResponseEntity.ok(new ResponseWrapper("Address is successfully created", addressDTO, HttpStatus.CREATED));
     }
 
     @GetMapping("/startsWith/{address}")
     public ResponseEntity<ResponseWrapper> getAddressListByStartsWithAddress(@PathVariable("address") String pattern) {
-        return ResponseEntity.ok(new ResponseWrapper("Address are successfully retrieved", addressService.getAddressStartsWith(pattern)));
+        return ResponseEntity.ok(new ResponseWrapper("Addresses are successfully retrieved", addressService.getAddressStartsWith(pattern), HttpStatus.OK));
     }
 
     @GetMapping("/customer/{id}")
     public ResponseEntity<ResponseWrapper> getAddressListByCustomerId(@PathVariable("id") long customerId) {
-        return ResponseEntity.ok(new ResponseWrapper("Address are successfully retrieved", addressService.getAddressByCustomerId(customerId)));
+        return ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved", addressService.getAddressByCustomerId(customerId), HttpStatus.OK));
     }
 
     @GetMapping("/customer/{customerId}/name/{name}")
     public ResponseEntity<ResponseWrapper> getAddressListByCustomerAndName(@PathVariable("customerId") long customerId, @PathVariable("name") String name) {
-        return ResponseEntity.ok(new ResponseWrapper("Address are successfully retrieved", addressService.getAddressByCustomerIdAndName(customerId, name)));
+        return ResponseEntity.ok(new ResponseWrapper("Address are successfully retrieved", addressService.getAddressByCustomerIdAndName(customerId, name), HttpStatus.OK));
     }
 
 
