f8cd145c2d25dcf9821945e3deb642d27b34ffb4
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-03 17:19:31 -0400

    added customer service

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CustomerService.java
@@ -0,0 +1,13 @@
+package com.cydeo.lab08rest.service;
+
+import com.cydeo.lab08rest.dto.CustomerDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<CustomerDTO> getAllCustomer();
+    CustomerDTO updateCustomer(CustomerDTO customerDTO);
+    CustomerDTO createCustomer(CustomerDTO customerDTO);
+    CustomerDTO getCustomerByEmail(String email);
+    CustomerDTO findById(Long id);
+}
