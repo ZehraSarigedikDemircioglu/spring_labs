833966eb9ce2617ce82d2041c13eeaf6577192b7
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-11 16:21:09 -0400

    added order controller

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
@@ -1,14 +1,15 @@
 package com.cydeo.lab08rest.controller;
 
-import com.cydeo.lab08rest.dto.AddressDTO;
 import com.cydeo.lab08rest.dto.OrderDTO;
+import com.cydeo.lab08rest.dto.UpdateOrderDTO;
 import com.cydeo.lab08rest.enums.PaymentMethod;
 import com.cydeo.lab08rest.model.ResponseWrapper;
-import com.cydeo.lab08rest.service.AddressService;
 import com.cydeo.lab08rest.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/order")
@@ -26,20 +27,27 @@ public class OrderController {
         return ResponseEntity
                 .ok(new ResponseWrapper("Orders are successfully retrieved", orderService.getAllOrders(), HttpStatus.OK));
     }
+
     @PutMapping
-    public ResponseEntity<ResponseWrapper> updateOrder(@RequestBody OrderDTO orderDTO) {
+    public ResponseEntity<ResponseWrapper> updateOrder(@Valid @RequestBody OrderDTO orderDTO) {
         orderService.updateOrder(orderDTO);
         return ResponseEntity.ok(new ResponseWrapper("Order is successfully updated", orderDTO, HttpStatus.CREATED));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseWrapper> updateOrderById(@PathVariable("id") Long id, @Valid @RequestBody UpdateOrderDTO updateOrderDTO) {
+
+        return ResponseEntity.ok(new ResponseWrapper("Order is successfully updated", orderService.updateOrderById(id, updateOrderDTO), HttpStatus.OK));
     }
 
     @PostMapping
     public ResponseEntity<ResponseWrapper> createOrder(@RequestBody OrderDTO orderDTO) {
         orderService.createOrder(orderDTO);
-        return ResponseEntity.ok(new ResponseWrapper("Address is successfully created", orderDTO, HttpStatus.CREATED));
+        return ResponseEntity.ok(new ResponseWrapper("Order is successfully created", orderDTO, HttpStatus.CREATED));
     }
 
     @GetMapping("/paymentMethod/{paymentMethod}")
-    public ResponseEntity<ResponseWrapper> getOrderByPaymentMethod(@PathVariable("paymentMethod")PaymentMethod paymentMethod) {
+    public ResponseEntity<ResponseWrapper> getOrderByPaymentMethod(@PathVariable("paymentMethod") PaymentMethod paymentMethod) {
         return ResponseEntity.ok(new ResponseWrapper("Payment method is successfully retrieved", orderService.getOrderByPaymentMethod(paymentMethod), HttpStatus.OK));
     }
 
