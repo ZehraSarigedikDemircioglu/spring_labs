b1a7bf218dd2cb138b8719dc93473f779141e96e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-11 16:28:11 -0400

    added exceptionWrapper custom class

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/exception/ExceptionWrapper.java
@@ -0,0 +1,26 @@
+package com.cydeo.lab08rest.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ExceptionWrapper {
+
+    // customize exception class. No need to go with the same Responsewrapper
+    public String message;
+    public HttpStatus httpStatus;
+
+    private Integer errorCount;
+    private List<ValidationError> validationErrorList;
+
+    public ExceptionWrapper(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
