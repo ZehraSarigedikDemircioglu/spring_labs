238600d61025c9a6719081c4d953832d76babdee
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-18 17:28:29 -0400

    added LocalDateTime timestamp field

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/exception/ExceptionWrapper.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/exception/ExceptionWrapper.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -15,7 +16,7 @@ public class ExceptionWrapper {
     // customize exception class. No need to go with the same Responsewrapper
     public String message;
     public HttpStatus httpStatus;
-
+    private LocalDateTime timestamp;
     private Integer errorCount;
     private List<ValidationError> validationErrorList;
 
