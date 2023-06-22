f8af10738a79c03cc668341ae297c770110d744e
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-22 15:55:54 -0400

    added notes

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/enums/DiscountType.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/enums/DiscountType.java
@@ -2,4 +2,8 @@ package com.cydeo.lab08rest.enums;
 
 public enum DiscountType {
     AMOUNT_BASED, RATE_BASED
+
+    // assume amount based => $50 if $500 minimum. So, for example $600 purchase, then $50 discount => $550
+
+    // assume rate based => $300 minimum, and %20 discount. So, for example $600 purchase, then %20 discount => $480
 }
