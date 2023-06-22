61beafb6db675b95fcb8e667e4ed53bb7b472a58
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-22 15:58:22 -0400

    added calculateTotalCartAmount method

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CartService.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/CartService.java
@@ -1,5 +1,20 @@
 package com.cydeo.lab08rest.service;
 
+import com.cydeo.lab08rest.dto.CartDTO;
+import com.cydeo.lab08rest.entity.Cart;
+import com.cydeo.lab08rest.entity.CartItem;
+import com.cydeo.lab08rest.entity.Customer;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface CartService {
     boolean existById(Long cartId);
+    CartDTO findById(Long id);
+
+    boolean addToCart(Customer customer, Long productId, Integer quantity);
+
+    BigDecimal applyDiscountToCartIfApplicableAndCalculateDiscountAmount(String discountName, Cart cart);
+
+    BigDecimal calculateTotalCartAmount(List<CartItem> cartItemList);
 }
