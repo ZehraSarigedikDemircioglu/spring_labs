bf2e93175457746b6b1555ba85a5f52fe2c78e28
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-22 15:54:34 -0400

    added place order method impl

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/impl/OrderServiceImpl.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/impl/OrderServiceImpl.java
@@ -3,13 +3,14 @@ package com.cydeo.lab08rest.service.impl;
 import com.cydeo.lab08rest.client.CurrencyApiClient;
 import com.cydeo.lab08rest.dto.OrderDTO;
 import com.cydeo.lab08rest.dto.UpdateOrderDTO;
-import com.cydeo.lab08rest.entity.Order;
+import com.cydeo.lab08rest.entity.*;
+import com.cydeo.lab08rest.enums.CartState;
 import com.cydeo.lab08rest.enums.Currency;
 import com.cydeo.lab08rest.enums.PaymentMethod;
 import com.cydeo.lab08rest.exception.CurrencyNotFoundException;
 import com.cydeo.lab08rest.exception.NotFoundException;
 import com.cydeo.lab08rest.mapper.MapperUtil;
-import com.cydeo.lab08rest.repository.OrderRepository;
+import com.cydeo.lab08rest.repository.*;
 import com.cydeo.lab08rest.service.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import java.math.RoundingMode;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -35,14 +37,22 @@ public class OrderServiceImpl implements OrderService {
     private final CartService cartService;
     private final PaymentService paymentService;
     private final CurrencyApiClient currencyApiClient;
+    private final CustomerRepository customerRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    private final PaymentRepository paymentRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository, MapperUtil mapperUtil, CustomerService customerService, CartService cartService, PaymentService paymentService, CurrencyApiClient currencyApiClient) {
+    public OrderServiceImpl(OrderRepository orderRepository, MapperUtil mapperUtil, CustomerService customerService, CartService cartService, PaymentService paymentService, CurrencyApiClient currencyApiClient, CustomerRepository customerRepository, CartRepository cartRepository, CartItemRepository cartItemRepository, PaymentRepository paymentRepository) {
         this.orderRepository = orderRepository;
         this.mapperUtil = mapperUtil;
         this.customerService = customerService;
         this.cartService = cartService;
         this.paymentService = paymentService;
         this.currencyApiClient = currencyApiClient;
+        this.customerRepository = customerRepository;
+        this.cartRepository = cartRepository;
+        this.cartItemRepository = cartItemRepository;
+        this.paymentRepository = paymentRepository;
     }
 
     @Override
@@ -55,7 +65,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderDTO> getOrderByEmail(String email) {
-        return null;
+        return orderRepository.findAllByCustomer_Email(email).stream()
+                .map(order -> mapperUtil.convert(order, new OrderDTO())).collect(Collectors.toList());
     }
 
     @Override
@@ -157,6 +168,98 @@ public class OrderServiceImpl implements OrderService {
         return mapperUtil.convert(order,new OrderDTO());
     }
 
+    // Now, it's time to pay, show us you really want it, Also you get your dream discount. Well deserved.
+    // You have selected desired product. We will have customer's money and place the order.
+    // This method responsibility is to place the shiny order.
+    // After that you can have a fresh air and refresh the page to see your order is shipped.
+    // But first we have to accept the order. This method does that.
+    @Override
+    public BigDecimal placeOrder(PaymentMethod paymentMethod, Long cartId, Long customerId) {
+
+        // we retrieve customer from DB, if not exists we need to throw exception
+        Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new RuntimeException("Customer couldn't find"));
+
+        // if a customer would like to place an order, cart should be created before.
+        List<Cart> cartList = cartRepository.findAllByCustomerIdAndCartState(customer.getId(), CartState.CREATED);
+        // if there is no cart in DB, we need to throw exception
+        // Once customer place order cart status will be SOLD and after that if customer would like to buy something
+        // again a new cart will be created. That's why a customer can have multiple carts but only one cart with CREATE status
+        // can be exist run time. All other carts should be SOLD status
+        if (cartList == null || cartList.size() == 0) {
+            throw new RuntimeException("Cart couldn't find or cart is empty");
+        }
+
+        // according to business requirement there always be 1 cart with created state
+        // That's why we are retrieving first index of cart list
+        Cart cart = cartList.get(0);
+
+        // We retrieve cart items in the cart
+        List<CartItem> cartItemList = cartItemRepository.findAllByCart(cartList.get(0));
+
+        // if there is an item quantity that exceeds product remains quantity, we have to remove it from cart item list;
+        cartItemList.removeIf(cartItem ->
+                cartItem.getQuantity() > cartItem.getProduct().getRemainingQuantity());
+        // if there is no item in the cart we are returning ZERO because this method should be returned paid price.
+        // No item means you can not pay anything
+        if (cartItemList.size() == 0){
+            return BigDecimal.ZERO;
+        }
+
+        // Discounts can be applied to cart but it is not mandatory. At first discount amount will be ZERO
+        // If a discount can be applicable to cart, we will have discounted amount depends on discount
+        // Before placing order discount must have been applied to cart.
+        BigDecimal lastDiscountAmount = BigDecimal.ZERO;
+        if (cart.getDiscount() != null) {
+            lastDiscountAmount = cartService.applyDiscountToCartIfApplicableAndCalculateDiscountAmount(cart.getDiscount().getName(), cart);
+        }
+
+        // we are calculating the cart total amount to have gross amount
+        BigDecimal totalCartAmount = calculateTotalCartAmount(cartItemList);
+
+        Payment payment = new Payment();
+        Order order = new Order();
+
+        order.setCart(cart);
+        order.setCustomer(customer);
+        order.setTotalPrice(totalCartAmount);
+
+        // total price $600
+        // after discount = $50 -> $550
+
+        // we are calculating te cart total amount after discount
+        order.setPaidPrice(totalCartAmount.subtract(lastDiscountAmount));
+
+
+        // let's assume if you pay with credit card we deduct 10 $ during the campaign period (Reward!!!)
+        // additional discount for specific payment method for credit card
+        if (paymentMethod.equals(PaymentMethod.CREDIT_CARD)) {
+            order.setPaidPrice(order.getPaidPrice().subtract(BigDecimal.TEN));
+        }
+
+        // initialising payment entity
+        // in the recordings, initialising payment was not inserted database directly.
+        // But it caused other problems so i decided to insert it into DB first
+        // After that we are setting payment value to Order Entity.
+        payment.setPaidPrice(order.getPaidPrice());
+        payment.setPaymentMethod(paymentMethod);
+        payment = paymentRepository.save(payment);
+        order.setPayment(payment);
+
+        // after successful order we have decrease product remaining quantity
+        // this stream is subtracting cart item quantity from product remaining quantity
+        cartItemList.forEach(cartItem -> {
+            cartItem.getProduct().setRemainingQuantity(
+                    cartItem.getProduct().getRemainingQuantity() - cartItem.getQuantity());
+            cartItemRepository.save(cartItem);
+        });
+
+        // stock is 50
+        // customer bought 18
+        // new stock will be 32
+        orderRepository.save(order);
+        return order.getPaidPrice();
+    }
+
     private void validateCurrency(String curr) {
         // check if the currency is valid currency
         List<String> currencies = Stream.of(Currency.values())
@@ -184,6 +287,16 @@ public class OrderServiceImpl implements OrderService {
         // check if currencyRate is valid
         return currencyRate;
     }
+    private BigDecimal calculateTotalCartAmount(List<CartItem> cartItemList) {
 
+        // this stream basically calculates the cart total amount depends on how many product is added to cart and theirs quantity
+        // there is also another same method that calculate cart total amount in CartService we should use it for readability
+        // but I paste it here to be able to show you more test cases.
+        Function<CartItem, BigDecimal> totalMapper = cartItem -> cartItem.getProduct().getPrice()
+                .multiply(BigDecimal.valueOf(cartItem.getQuantity()));
 
+        return cartItemList.stream()
+                .map(totalMapper)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
 }
