e4d56ab6b046b6d2dd212062ec2fdb54586761eb
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-06-26 15:06:56 -0400

    added findAllByCart method

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/repository/OrderRepository.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.cydeo.lab08rest.repository;
 
+import com.cydeo.lab08rest.entity.Cart;
 import com.cydeo.lab08rest.entity.Order;
 import com.cydeo.lab08rest.enums.PaymentMethod;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -46,5 +47,7 @@ public interface OrderRepository extends JpaRepository<Order,Long> {
 
     @Query("SELECT o FROM Order o WHERE o.paidPrice<>o.totalPrice AND o.cart.discount IS NOT NULL")
     List<Order> findAllByPaidPriceAndTotalPriceAEqualsAndCartDiscountIdIsNull();
+
+    Order findAllByCart(Cart cart);
 
 }
